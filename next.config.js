74854030d233cc7704cf3bb278e43efa779c6133
lucas <lafdesouza2002@outlook.com>
2025-11-02 22:16:37 -0300

    Add Prisma plugin support and experimental features in next.config.js; update package.json with new dependencies

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+
 // @ts-check
  
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    devIndicators: false,
+    experimental: {
+        authInterrupts: true,
+    },
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            config.plugins = [...config.plugins, new PrismaPlugin()];
+        }
+        return config;
+    },
     images: {
         remotePatterns: [
             {
