e2163942e745c05d744598d2db1f2d63e07f4b2c
lucas <lafdesouza2002@outlook.com>
2025-11-03 09:55:31 -0300

    Add output file tracing includes for Prisma client in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -23,6 +23,10 @@ const nextConfig = {
             },
           ],
     },
+    outputFileTracingIncludes: {
+      '/api/**/*': ['./node_modules/.prisma/client/**/*'],
+      '/*': ['./node_modules/.prisma/client/**/*'],
+    },
   }
    
 export default nextConfig
