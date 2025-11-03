6349f059384e0f5647ed9fa7052019126b1b7ee5
lucas <lafdesouza2002@outlook.com>
2025-11-02 21:59:52 -0300

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,15 @@
+// @ts-check
+ 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+              protocol: "https",
+              hostname: "**",
+            },
+          ],
+    },
+  }
+   
+module.exports = nextConfig
